da824a9173e413a714dfebbbf854ff8ab4a2cb2e
Ciaran Edwards <ciaran.edwards@live.com>
2018-05-28 13:42:31 +0100

    Add dropdown menus in the navbar

--- app.js
+++ app.js
@@ -8,19 +8,30 @@ var apos = require('apostrophe')({
   // configuring user acounts.
 
   modules: {
-
     // Apostrophe module configuration
 
     // Note: most configuration occurs in the respective
     // modules' directories. See lib/apostrophe-assets/index.js for an example.
-    
+
     // However any modules that are not present by default in Apostrophe must at
     // least have a minimal configuration here: `moduleName: {}`
 
     // If a template is not found somewhere else, serve it from the top-level
     // `views/` folder of the project
+    'apostrophe-templates': {
+      viewsFolderFallback: path.join(__dirname, 'views'),
+    },
 
-    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') }
+    'apostrophe-pages': {
+      filters: {
+        // Grab our ancestor pages, with two levels of subpages
+        ancestors: { children: { depth: 2 } },
+        // We usually want children of the current page, too
+        children: true,
+      },
+      // other apostrophe-pages options like `types` ...
+    },
 
-  }
+    'link-widgets': {},
+  },
 });
